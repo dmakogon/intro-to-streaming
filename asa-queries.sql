68c728468e45507fc2cb911f9512d708d913c6ff
David Makogon <dmakogon@gmail.com>
2019-06-07 08:12:50 -0400

    tweaked sample queries

--- asa-queries.sql
+++ asa-queries.sql
@@ -1,6 +1,6 @@
 -- Hello World of streaming
 SELECT *
-INTO stats
+INTO alldata
 FROM donutshop
 
 -- Grab specific fields
@@ -8,42 +8,57 @@ SELECT
     id,
     eventTime,
     donutType,
-    qcPassed,
-    'sample' as statType
+    qcIssueCount,
+    partitionId
 INTO
-    stats
+    filtereddata
 FROM
     donutshop TIMESTAMP BY eventTime
 
--- simple aggregation: number of samples in a 15 second period
+-- simple aggregation: number of samples, and all sums, in a 15 second period
 SELECT
-    COUNT(*) as DonutCount,
-    'summaryCount' as statType
+    COUNT(*) as 'sampleCount',
+    SUM(donutCount) as 'donutCount',
+    SUM(qcIssueCount) as 'qcIssueCount',
+    System.TimeStamp AS 'windowEnd'
 INTO
-    stats
+    aggregatedcounts
 FROM
     donutshop TIMESTAMP BY eventTime
 GROUP BY TumblingWindow(second,15)
 
 -- donut counts per minute, by type
-SELECT donutType, SUM(donutCount)
-INTO stats
+SELECT donutType, SUM(donutCount),
+System.TimeStamp AS 'windowEnd'
+INTO aggregatedcounts 
 FROM donutshop TIMESTAMP by eventTime
-GROUP BY TumblingWindow(minute,1),donutType
+GROUP BY TumblingWindow(minute,1),donutType, windowEnd
 
 -- sum donut counts and measurement counts, by store
 SELECT
     storeId,
-    '1' as 'partitionId',
+    partitionId,
     System.TimeStamp AS 'windowEnd',
     SUM(donutCount) as 'donutCount',
-    COUNT(*) as 'measurementCount',
-    'storeSummaryCount' as 'statType'
+    COUNT(*) as 'measurementCount'
 INTO
     [summary]
 FROM
     [donutshop]
 GROUP BY TumblingWindow(minute,1),storeId
+
+-- Look for quality control issues
+-- In this example, flag stores having more than 1 issue in a 30 second window
+SELECT
+    storeId,
+    System.TimeStamp AS 'windowEnd',
+    SUM(qcIssueCount) as 'qcIssueCount'
+INTO
+    [summary]
+FROM
+    [donutshop] TIMESTAMP BY eventTime
+GROUP BY storeId,SlidingWindow(second,30)
+HAVING qcIssueCount > 20
 
 /* maybe have multiple sensors in a conveyor belt?
  - start
